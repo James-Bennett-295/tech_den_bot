d82c32e136427aa5789ac9e18816fa53fa108d91
James Bennett <JamesBennett25565@gmail.com>
2022-03-15 18:27:56 +0000

    Added balance command

---
+++ src/cmds/economy/balance.js
@@ -0,0 +1,17 @@
+"use strict";
+
+module.exports = {
+	name: "balance",
+	minArgs: 0,
+	usage: null,
+	cooldown: 2000,
+	description: "See how many coins you have!",
+	category: "Economy",
+	botOwnerOnly: false,
+	execute(cfg, client, db, msg, args) {
+
+		db.add("balance." + msg.author.id, 0); // so balance.<user> will be created if doesn't exist
+        msg.reply("Your balance: `" + db.get("balance." + msg.author.id) + "`");
+
+	},
+};
