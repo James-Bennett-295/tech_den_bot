97e0a3c8e594a797d7b4c5e04ea0adee7cc5c95a
James Bennett <JamesBennett25565@gmail.com>
2022-03-15 19:10:49 +0000

    Added getpp cmd

---
+++ src/cmds/fun/getpp.js
@@ -0,0 +1,16 @@
+"use strict";
+
+module.exports = {
+	name: "getpp",
+	minArgs: 0,
+	usage: null,
+	cooldown: 1000,
+	description: "Get your pp size!",
+	category: "Fun",
+	botOwnerOnly: false,
+	execute(cfg, client, db, msg, args) {
+
+		msg.reply("Your PP:\t8" + "=".repeat(Math.floor(Math.random() * 12) + 1) + "D");
+
+	},
+};
