a7ff024027f6ec8ff583eacffae16cd43ceeac76
James Bennett <JamesBennett25565@gmail.com>
2022-03-16 18:02:11 +0000

    Added eval cmd

---
+++ src/cmds/util/eval.js
@@ -0,0 +1,20 @@
+"use strict";
+
+module.exports = {
+	name: "eval",
+	minArgs: 1,
+	usage: "<code>",
+	cooldown: 0,
+	description: "Eval JS code.",
+	category: "Utility",
+	botOwnerOnly: true,
+	execute(cfg, client, db, msg, args) {
+
+		let code = args.join(' ');
+
+        let evaled = eval(code);
+
+        msg.reply(evaled.toString().slice(0, 2000));
+
+	},
+};
