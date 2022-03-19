72ad54fe3df3e69994f813fd7a28ae7da2b952a2
James Bennett <JamesBennett25565@gmail.com>
2022-03-19 15:22:27 +0000

    Added xkcd comd

---
+++ src/cmds/fun/xkcd.js
@@ -0,0 +1,33 @@
+"use strict";
+
+const xkcd = require("xkcd-wrapper");
+const logger = require("@james-bennett-295/logger");
+const discord = require("discord.js");
+
+module.exports = {
+    name: "xkcd",
+    minArgs: 0,
+    usage: null,
+    cooldown: 5000,
+    description: "Get a random post from xkcd.com!",
+    category: "Fun",
+    botOwnerOnly: false,
+    execute(cfg, client, db, msg, args) {
+
+        xkcd.getRandom()
+            .then((obj) => {
+                let embed = new discord.MessageEmbed()
+                    .setColor("AQUA")
+                    .setTitle(obj.title)
+                    .setURL(obj.link)
+                    .setDescription(obj.alt)
+                    .setImage(obj.imgUrl)
+                    .setFooter({ text: "Source: xkcd.com" });
+                msg.reply({ embeds: [embed] }).catch((e) => {});
+            })
+            .catch((err) => {
+                logger.error("[xkcd cmd]: xkcd.getRandom() errored: " + err);
+            });
+
+    },
+};
