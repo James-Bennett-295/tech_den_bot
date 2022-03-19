5245cb3e0f12feb947f035f35d9f7beb9da16909
James Bennett <JamesBennett25565@gmail.com>
2022-03-19 15:51:43 +0000

    Allow findImg util to get images from embeds

--- src/util/findImg.js
+++ src/util/findImg.js
@@ -11,8 +11,12 @@ function findImg(msg) {
                 let msgsArr = Array.from(messages.values());
                 for (let i in msgsArr) {
                     let firstAttachment = Array.from(msgsArr[i].attachments.values())[0];
-                    if (typeof(firstAttachment) === "undefined") continue;
-                    if (firstAttachment.contentType.startsWith("image/")) resolve(firstAttachment.attachment);
+                    if (typeof (firstAttachment) !== "undefined") {
+                        if (firstAttachment.contentType.startsWith("image/")) resolve(firstAttachment.attachment);
+                    };
+                    if (msgsArr[i].embeds.length > 0 && msgsArr[i].embeds[0].image) {
+                        resolve(msgsArr[i].embeds[0].image.proxyURL);
+                    };
                 };
                 reject("NO_IMAGE");
             });
