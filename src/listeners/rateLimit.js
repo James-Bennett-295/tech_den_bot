6cc48d242433bf2bb056f75c2fb63fbae911fe84
James Bennett <JamesBennett25565@gmail.com>
2022-03-20 19:11:23 +0000

    Added rateLimit listener and handler

---
+++ src/listeners/rateLimit.js
@@ -0,0 +1,13 @@
+"use strict";
+
+const logger = require("@james-bennett-295/logger");
+
+module.exports = {
+    name: "rateLimit",
+    once: false,
+    execute(cfg, client, db, info) {
+
+        logger.error("Discord rate-limit hit! INFO: " + JSON.stringify(info, null, 2));
+
+    },
+};
