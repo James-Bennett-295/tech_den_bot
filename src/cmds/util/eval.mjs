21cca1bb278498d49d0a1e740f954c1718780996
James Bennett <JamesBennett25565@gmail.com>
2022-04-15 21:46:46 +0100

    Make eval command better

--- src/cmds/util/eval.mjs
+++ src/cmds/util/eval.mjs
@@ -1,3 +1,6 @@
+import discord from "discord.js";
+import logger from "@james-bennett-295/logger";
+
 export default {
 	name: "eval",
 	minArgs: 1,
@@ -7,13 +10,41 @@ export default {
 	category: "Utility",
 	botOwnerOnly: true,
 	staffOnly: false,
-	execute: function(cfg, client, db, msg, args) {
+	execute: function (cfg, client, db, msg, args) {
 
 		let code = args.join(' ');
 
-		let evaled = eval(code);
+		if (code.startsWith("```js")) {
+			code = code.slice(5, -3);
+		}
 
-		msg.reply(evaled.toString().slice(0, 2000)).catch((e) => { });
+		try {
+			let evalOut = eval(code);
+			let evalOutStr, fileType;
+			switch (typeof evalOut) {
+				case "object":
+					evalOutStr = JSON.stringify(evalOut, null, "\t");
+					fileType = "json";
+					break;
+				case "undefined":
+					msg.reply("EVAL HAD NO OUTPUT");
+					return;
+				default:
+					evalOutStr = evalOut.toString();
+					fileType = "js";
+			}
+			let outFile = new discord.MessageAttachment(Buffer.from(evalOutStr), "EVAL-OUT." + fileType);
+			msg.reply({ content: "**[SUCCESS]**\nTYPE: " + typeof evalOut, files: [outFile] }).catch((err) => {
+				logger.error("[eval cmd]: " + err);
+				msg.reply("Failed to send eval output.");
+			});
+		} catch (err) {
+			let errFile = new discord.MessageAttachment(Buffer.from(err.toString()), "EVAL-ERR.js");
+			msg.reply({ content: "**[ERROR]**", files: [errFile] }).catch((err) => {
+				logger.error("[eval cmd]: " + err);
+				msg.reply("Failed to send eval error.");
+			});
+		}
 
 	}
 }
