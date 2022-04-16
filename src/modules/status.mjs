1e5c2e973b157a8d55d5c55a1640cf108ff32455
James Bennett <JamesBennett25565@gmail.com>
2022-04-16 23:33:01 +0100

    Changes to status module

--- src/modules/status.mjs
+++ src/modules/status.mjs
@@ -1,25 +1,39 @@
 import logger from "@james-bennett-295/logger";
 
+function setStatus(cfg, client, db, statusNum) {
+
+	if (typeof client.memberCount === "undefined") {
+		logger.warn("[status module]: Not setting status as client.memberCount is undefined");
+		return;
+	}
+
+	logger.debug("[status module]: Setting status " + statusNum);
+
+	client.user.setPresence({
+		activities: [
+			{
+				name: cfg.status.statuses[statusNum][1]
+					.replace("{userCount}", client.memberCount)
+				, type: cfg.status.statuses[statusNum][0]
+			}
+		],
+		status: "online"
+	});
+}
+
 function onReady(cfg, client, db) {
 
-	let statusNum = 0;
+	setTimeout(() => { // timeout to give time for client.memberCount to be set
+		setStatus(cfg, client, db, 0);
+	}, 2500);
+
+	let statusNum = 1;
 	setInterval(() => {
-		if (typeof client.memberCount === "undefined") {
-			logger.warn("[status module]: Not setting status as client.memberCount is undefined");
-			return;
-		}
+
 		if (cfg.status.statuses.length <= statusNum) statusNum = 0;
-		client.user.setPresence({
-			activities: [
-				{
-					name: cfg.status.statuses[statusNum][1]
-						.replace("{userCount}", client.memberCount)
-					, type: cfg.status.statuses[statusNum][0]
-				}
-			],
-			status: "online"
-		});
+		setStatus(cfg, client, db, statusNum);
 		statusNum++;
+
 	}, cfg.status.cycleTime * 1000);
 
 }
