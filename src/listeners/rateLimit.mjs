196fd2053747939f00ab169d6d7b40fc972f1b41
James Bennett <JamesBennett25565@gmail.com>
2022-04-16 23:04:36 +0100

    Make rate limit msg log as warn instead of error

--- src/listeners/rateLimit.mjs
+++ src/listeners/rateLimit.mjs
@@ -5,7 +5,7 @@ export default {
 	once: false,
 	execute: function(cfg, client, db, info) {
 
-		logger.error("Discord rate-limit hit! INFO: " + JSON.stringify(info, null, 2));
+		logger.warn("Discord rate-limit hit! INFO: " + JSON.stringify(info, null, 2));
 
 	}
 }
