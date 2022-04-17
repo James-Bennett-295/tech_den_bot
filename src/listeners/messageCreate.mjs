aaee1645701ab9e28ada567fa59f25df8a2e0fdf
James Bennett <JamesBennett25565@gmail.com>
2022-04-17 16:53:35 +0100

    Fix bump reminder

--- src/listeners/messageCreate.mjs
+++ src/listeners/messageCreate.mjs
@@ -3,12 +3,14 @@ export default {
   once: false,
   execute: function (cfg, client, db, msg) {
 
-    if (msg.author.bot || msg.webhookId) return;
     if (msg.guild === null) return;
+
+    client.modules.bumpReminder.onMessageCreate(cfg, client, db, msg);
+
+    if (msg.author.bot || msg.webhookId) return;
 
     client.modules.cmds.onMessageCreate(cfg, client, db, msg);
     client.modules.msgRewards.onMessageCreate(cfg, client, db, msg);
-    client.modules.bumpReminder.onMessageCreate(cfg, client, db, msg);
 
   }
 }
--- src/listeners/messageDelete.mjs
+++ src/listeners/messageDelete.mjs
@@ -3,8 +3,8 @@ export default {
 	once: false,
 	execute: function (cfg, client, db, msg) {
 
-		if (msg.author.bot || msg.webhookId) return;
 		if (msg.guild === null) return;
+		if (msg.author.bot || msg.webhookId) return;
 
 		client.modules.msgDeleteLog.onMessageDelete(cfg, client, db, msg);
 
