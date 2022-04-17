c5cb14ef102fe8971dc69166fdbca5d6591af966
James Bennett <JamesBennett25565@gmail.com>
2022-04-17 16:20:50 +0100

    ignore messages outside of guilds

--- src/listeners/messageCreate.mjs
+++ src/listeners/messageCreate.mjs
@@ -4,6 +4,7 @@ export default {
   execute: function (cfg, client, db, msg) {
 
     if (msg.author.bot || msg.webhookId) return;
+    if (msg.guild === null) return;
 
     client.modules.cmds.onMessageCreate(cfg, client, db, msg);
     client.modules.msgRewards.onMessageCreate(cfg, client, db, msg);
--- src/listeners/messageDelete.mjs
+++ src/listeners/messageDelete.mjs
@@ -4,6 +4,7 @@ export default {
 	execute: function (cfg, client, db, msg) {
 
 		if (msg.author.bot || msg.webhookId) return;
+		if (msg.guild === null) return;
 
 		client.modules.msgDeleteLog.onMessageDelete(cfg, client, db, msg);
 
