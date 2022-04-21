a5850cd98ac82464a8676e4e69434d845c22bddd
James Bennett <JamesBennett25565@gmail.com>
2022-04-21 21:46:05 +0100

    Added "fromfahrenheit" cmd

---
+++ src/cmds/conversion/fromfahrenheit.mjs
@@ -0,0 +1,24 @@
+export default {
+	name: "fromfahrenheit",
+	minArgs: 1,
+	usage: "<temperature (fahrenheit)>",
+	cooldown: 1000,
+	description: "Convert temperature from fahrenheit to celsius",
+	category: "Conversion",
+	botOwnerOnly: false,
+	staffOnly: false,
+	execute: function (cfg, client, db, msg, args) {
+
+		if (isNaN(args[0])) {
+			return msg.reply("Temperature inputted is not a valid number.").catch((e) => { });
+		}
+
+		let fahrTmp = parseInt(args[0]);
+		let celsTmp = (fahrTmp - 32) * 5/9;
+
+		celsTmp = Math.ceil(celsTmp * 100000) / 100000; // Round to 5 decimal places
+
+		msg.reply(celsTmp + "\u00b0C");
+
+	}
+}
