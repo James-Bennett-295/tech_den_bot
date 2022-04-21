4aef57eea6b1ad76c90039341a2598ca2481003d
James Bennett <JamesBennett25565@gmail.com>
2022-04-21 19:47:51 +0100

    Added unixtimestamp cmd

---
+++ src/cmds/conversion/unixtimestamp.mjs
@@ -0,0 +1,28 @@
+export default {
+	name: "unixtimestamp",
+	minArgs: 1,
+	usage: "<unix timestamp>",
+	cooldown: 1000,
+	description: "Convert a unix timestamp to an ISO date.",
+	category: "Conversion",
+	botOwnerOnly: false,
+	staffOnly: false,
+	execute: function (cfg, client, db, msg, args) {
+
+		if (isNaN(args[0])) {
+			return msg.reply("Unix timestamp input is not a valid number.").catch((e) => { });
+		}
+
+		const timestamp = parseInt(args[0]);
+		const date = new Date(timestamp * 1000);
+		let isoDate;
+		try {
+			isoDate = date.toISOString();
+		} catch (e) {
+			return msg.reply("Invalid time value entered.").catch((e) => { })
+		}
+
+		msg.reply("`" + isoDate + "`").catch((e) => { });
+
+	}
+}
