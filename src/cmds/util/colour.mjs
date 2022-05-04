33a2620cf6af98076644eac8140337e6dbd76c11
James Bennett <JamesBennett25565@gmail.com>
2022-05-04 16:01:05 +0100

    fix colour cmd

--- src/cmds/util/colour.mjs
+++ src/cmds/util/colour.mjs
@@ -11,27 +11,27 @@ export default {
 	category: "Utility",
 	botOwnerOnly: false,
 	staffOnly: false,
-	execute: function(cfg, client, db, msg, args) {
+	execute: function (cfg, client, db, msg, args) {
 
 		let hex = args[0];
 
-		if (hex.charAt(0) === '#') hex = hex.slice(1, hex.length - 1);
+		if (hex.charAt(0) === '#') hex = hex.slice(1, hex.length);
 
 		if (!/^[0-9a-fA-F]{6}$/.test(hex)) {
 			return msg.reply("Invalid hex number!");
 		}
 
-		let num = parseInt(hex, 16);
+		const num = parseInt(hex, 16);
 
 		if (isNaN(num) || num < 0 || 16777215 < num) {
 			return msg.reply("Invalid colour!");
 		}
 
-		let r = (num >> 16) & 255;
-		let g = (num >> 8) & 255;
-		let b = num & 255;
+		const r = (num >> 16) & 255;
+		const g = (num >> 8) & 255;
+		const b = num & 255;
 
-		let font = (r * 0.2126) + (g * 0.7152) + (b * 0.0722) < 128 ? jimp.FONT_SANS_32_WHITE : jimp.FONT_SANS_32_BLACK;
+		const font = (r * 0.2126) + (g * 0.7152) + (b * 0.0722) < 128 ? jimp.FONT_SANS_32_WHITE : jimp.FONT_SANS_32_BLACK;
 
 		sharp({
 			create: {
@@ -52,7 +52,7 @@ export default {
 								.print(jFont, 8, 8, '#' + hex.toUpperCase())
 								.getBufferAsync("image/png")
 								.then((tBuf) => {
-									let file = new discord.MessageAttachment(tBuf, hex.toLowerCase() + ".png");
+									const file = new discord.MessageAttachment(tBuf, hex.toLowerCase() + ".png");
 									msg.reply({ files: [file] }).catch((e) => { });
 								})
 						});
