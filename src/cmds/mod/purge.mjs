69dfaece76a803358e03080cb1dd7efca68b1045
James Bennett <JamesBennett25565@gmail.com>
2022-05-19 21:52:48 +0100

    Make purge command use bulkDelete

--- src/cmds/mod/purge.mjs
+++ src/cmds/mod/purge.mjs
@@ -20,29 +20,13 @@ export default {
 			return msg.reply("The message count must be between 2 and 99.").catch((e) => { });
 		}
 
-		msg.react("\ud83d\udd01"); // :reload:
 		msg.channel.messages.fetch({ limit: msgCount + 1 })
 			.then((fetchedMessages) => {
 
 				const mentionedUsers = msg.mentions.users.map(u => { return u.id; });
 				const messages = fetchedMessages.filter(m => mentionedUsers.length === 0 || mentionedUsers.includes(m.author.id));
-				let index = 0;
-				let cmdMsg = {};
 
-				messages.forEach((message) => {
-					if (index === 0) {
-						Object.assign(cmdMsg, message);
-						index = 1;
-						return;
-					}
-					message.delete().catch((e) => { });
-					index++;
-				});
-
-				msg.react("\u2705"); // :white_check_mark:
-				setTimeout(() => {
-					msg.delete().catch((e) => { });
-				}, 2500);
+				msg.channel.bulkDelete(messages);
 
 			}).catch((err) => {
 				logger.error("[purge cmd]: An error occured while trying to fetch messages: " + err);
