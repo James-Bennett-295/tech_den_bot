da673583bacb87b5384fc1cf203ba64405dec872
James Bennett <JamesBennett25565@gmail.com>
2022-05-20 16:36:25 +0100

    Add purgeuntil cmd

---
+++ src/cmds/mod/purgeuntil.mjs
@@ -0,0 +1,37 @@
+import logger from "@james-bennett-295/logger";
+
+const msgIdPattern = /^[0-9]{18}$/;
+
+export default {
+	name: "purgeuntil",
+	minArgs: 1,
+	usage: "<id of message to stop before>",
+	cooldown: 6000,
+	description: "Clear up to before a certain message.",
+	category: "Moderation",
+	botOwnerOnly: false,
+	staffOnly: true,
+	execute: function (cfg, client, db, msg, args) {
+
+		if (!msgIdPattern.test(args[0])) {
+			return msg.reply("Invalid message ID provided").catch((e) => { });
+		}
+
+		msg.channel.messages.fetch({ limit: 100 })
+			.then((messages) => {
+
+				const messagesKeys = Array.from(messages.keys());
+
+				for (let i = messagesKeys.length - 1; i > 0; i--) {
+					if (messages.get(messagesKeys[i]).id === args[0]) break;
+					messages.delete(messagesKeys[i]);
+				}
+
+				msg.channel.bulkDelete(messages);
+
+			}).catch((err) => {
+				logger.error("[purgeuntil cmd]: An error occured while trying to fetch messages: " + err);
+			});
+
+	}
+}
