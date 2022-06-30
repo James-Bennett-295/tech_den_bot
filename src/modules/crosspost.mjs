0cedb0c237e490b628c0a0342e4ab59f3efaeb76
James Bennett <JamesBennett25565@gmail.com>
2022-06-30 16:31:58 +0100

    Make crosspost module only try and crosspost the message if the msg type is "DEFAULT"

--- src/modules/crosspost.mjs
+++ src/modules/crosspost.mjs
@@ -3,6 +3,9 @@ function onMessageCreate(cfg, client, db, msg) {
 	if (msg.content.startsWith(cfg.cmds.prefix)) return;
 
 	if (msg.channel.type === "GUILD_NEWS") {
+
+		if (msg.type !== "DEFAULT") return;
+
 		msg.crosspost();
 	}
 }
